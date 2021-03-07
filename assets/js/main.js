9558644b6eaa972aa863519747a4c313d005e370
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2021-03-08 01:05:50 +0300

    Added to bundle.js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,3 +78,35 @@ themeButton.addEventListener("click", () => {
   localStorage.setItem("selected-theme", getCurrentTheme());
   localStorage.setItem("selected-icon", getCurrentIcon());
 });
+
+function scaleCv() {
+  document.body.classList.add("scale-cv");
+}
+
+function removeScale() {
+  document.body.classList.remove("scale-cv");
+}
+
+let areaCv = document.getElementById("area-cv");
+
+let resumeButton = document.getElementById("resume-button");
+
+let opt = {
+  margin: 0,
+  filename: "myResume.pdf",
+  image: { type: "jpeg", quality: 0.98 },
+  html2canvas: { scale: 4 },
+  jsPDF: { format: "a4", orientation: "portrait" },
+};
+
+function generateResume() {
+  html2pdf(areaCv, opt);
+}
+
+resumeButton.addEventListener("click", () => {
+  scaleCv();
+
+  generateResume();
+
+  setTimeout(removeScale, 5000);
+});
